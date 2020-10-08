49da9c889b8480cfb3f746e905eef200ff28ddb9
aissam <hesokaslave@gmail.com>
2020-10-08 22:17:25 +0100

    fix start

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const serveStatic = require('serve-static');
 let app = express();
 
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(__dirname + "/build"));
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
