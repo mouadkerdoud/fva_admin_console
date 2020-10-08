91bba1a4511eafab0c9afa6252baf0b40b6aab57
aissam <hesokaslave@gmail.com>
2020-10-08 22:42:05 +0100

    deploy success

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const serveStatic = require('serve-static');
-let app = express();
-
-app.use(serveStatic(__dirname + "/build"));
-
+const express = require("express");
+// eslint-disable-next-line no-unused-vars
+// const bodyParser = require('body-parser');
+const path = require("path");
+const app = express();
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
-    console.log('Listening on port ' + port)
-});
+
+app.use(express.static(path.join(__dirname, "build")));
+
+// This route serves the React app
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
