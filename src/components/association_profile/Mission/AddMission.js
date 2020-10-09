b70c3be36f59bd6297a562a165e074c6a396c94d
Tarik Jebrouni <tarik.hani74@gmail.com>
2020-10-09 23:38:12 +0100

    add Mission Association

--- src/components/association_profile/Mission/AddMission.js
+++ src/components/association_profile/Mission/AddMission.js
@@ -8,7 +8,7 @@ import CKEditor from "@ckeditor/ckeditor5-react"
 
 export class AddMission extends React.Component {
     state={
-        profileImg:'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+        profileImg:''
       }
   imageHandler = (e) => {
     const reader = new FileReader();
@@ -22,37 +22,34 @@ export class AddMission extends React.Component {
     render(){
         return (
             <div className="container">
-                <h1>Add Mission</h1>
-    
-                <form class="add-product-form forms5">
-                    
-                    <div className="form-flex-items">
-                        <div class="main-menu">
-                            <h2 class="active"><i class="fa fa-table"></i>News</h2>     
+                <h1>Add User</h1>
+                <form className="assoc-form avatar">
+                    <div className="assoc-wrapper">
+                        <div className="row">
+                            <div className="field label-input mt">
+                                <label className="label">Mission Title*</label>
+                                <input placeholder="Title..." type="text" className="data-form add-post" />
+                            </div> 
+                            <div className="description-element label-input">
+                                <label className="label">Content*</label>
+                                <CKEditor
+                                            editor={ClassicEditor}
+                                            onInit={editor=>{
+                                            }}
+                                />
+                            </div>
                         </div>
-                    </div> 
-                    <div className="field label-input mt">
-                            <label className="label">Mission Title*</label>
-                            <input placeholder="Title..." type="text" className="data-form add-post" />
-                        </div> 
-                    <div className="description-element label-input">
-                            <label className="label">Content*</label>
-                            <CKEditor
-                                        editor={ClassicEditor}
-                                        onInit={editor=>{
-                                        }}
-                            />
+                        <div className="picture-assoc assoc">
+                            <div class="avatar-wrapper">
+                                <img class="profile-pic" src={this.state.profileImg} /> 
+                            </div>
+                            <input class="file-upload"  type="file" accept="image/*" onChange={this.imageHandler} />                                                             
+
                         </div>
-                        <div className="category-element label-input">
-                            
-                            
-                        </div>
+                    </div>    
                 </form>
-                 <form class="add-product-form formsbtn">
-                    <button className="btn btn-add-prod">Add Event</button>
-                </form>
-    
-            </div>
+            </div>    
+            
         )
     
     }    
