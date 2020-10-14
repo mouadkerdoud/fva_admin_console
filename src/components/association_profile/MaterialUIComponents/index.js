e2e31a1de914cd9d968fe2972eeff27f858e2104
Tarik Jebrouni <tarik.hani74@gmail.com>
2020-10-15 00:57:46 +0100

    modify section the setting association

--- src/components/association_profile/MaterialUIComponents/index.js
+++ src/components/association_profile/MaterialUIComponents/index.js
@@ -285,13 +285,210 @@ import { withStyles } from "@material-ui/core/styles";
                 />
         )
     } 
-    //METAS   
 
-    const styles = theme => ({
+    /* Setting */
+    export const AssociationName = () => {
+      
+        return (
+            <TextField
+              id="assname"
+              label="Association Name*"
+              type="text"
+              style={{ width: 270 }}
+              value="Future Vision Association"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+        );
+      }
+
+    export const EmailA = () => {
+      
+        return (
+            <TextField
+              id="email"
+              label="Email*"
+              type="email"
+              style={{ width: 270 }}
+              value="fva@gmail.com"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+        );
+      }
+    export const Phone = () => {
+      
+        return (
+            <TextField
+              id="number"
+              label="Phone Number*"
+              type="number"
+              style={{ width: 270 }}
+              value="0537849143"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+        );
+      }
+
+      export const Fax = () => {
+      
+        return (
+            <TextField
+              id="fax"
+              label="Fax"
+              type="number"
+              style={{ width: 270 }}
+              value="55487910"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+        );
+      }      
+
+      export const AddressA = () => {
+      
+        return (
+            <TextField
+              id="address"
+              label="Address*"
+              type="text"
+              style={{ width: 270 }}
+              value="Rue something, somewhere"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+        );
+      }
+
+      export const CountryA = () => {
+        const country = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+
+        return (
+           <>
+            <Autocomplete
+                id="countrya"
+                options={country}
+                value="Morocco"
+                style={{ width: 270 }}
+                renderInput={(params) => <TextField {...params} label="Country" variant="outlined" />}
+            />
+           </>
+
+        );
+      }
+
+      export const CityA = () => {
+        const city=["Tanger","Asila","Mekness","Rabat","Sale","Asfi","Marrakech","Agadir","Taza","Fes","Tetouane","Layounne","Khenifra","Kelmime","Ouzzane"];
+
+        return (
+            <>
+            <Autocomplete
+                    id="cityA"
+                    options={city}
+                    value="Rabat"
+                    style={{ width: 270 }}
+                    renderInput={(params) => <TextField {...params} label="City" variant="outlined" />}
+                />
+            </>
+        );
+      }
+
+      export const FacebookLink = () => {
+      
+        return (
+            <TextField
+              id="facebook"
+              label="Facebook Link*"
+              type="text"
+              style={{ width: 270 }}
+              value="fb/someassociation.com"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+        );
+      }
+
+      export const InstagramLink = () => {
+      
+        return (
+            <TextField
+              id="instagrame"
+              label="Instagram Link*"
+              type="text"
+              style={{ width: 270 }}
+              value="ig/someassociation.com"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+        );
+      }
+
+      export const LinkedinLink = () => {
+      
+        return (
+            <TextField
+              id="linkedin"
+              label="Linkedin Link*"
+              type="text"
+              style={{ width: 270 }}
+              value="lkdin/someassociation.com"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+        );
+      }
+
+      const styles = theme => ({
         input: {
         height: 120
         },
     });
+
+      export const AssociationDescription = withStyles(styles)(props =>  {
+        const { classes } = props;
+
+        return (
+            <TextField
+                id="associationd"
+                placeholder="blah blah blah"
+                multiline
+                style={{ width: 270 }}
+                variant="outlined"
+                rowsMax={4}
+                InputProps={{
+                    className: classes.input
+                }}
+            />
+        )
+    })
+
+    export const AboutUs = withStyles(styles)(props =>  {
+        const { classes } = props;
+
+        return (
+            <TextField
+                id="aboutUs"
+                placeholder="blah blah blah"
+                multiline
+                style={{ width: 270 }}
+                variant="outlined"
+                rowsMax={4}
+                InputProps={{
+                    className: classes.input
+                }}
+            />
+        )
+    })
+    //METAS   
 
 
     export const MetaName = () => {
