1142a57bac2d9708677939ab5c672015a9875a40
Tarik Jebrouni <tarik.hani74@gmail.com>
2020-10-21 15:32:29 +0100

    modify section Event and add list and delete to news

--- src/components/association_profile/News/News.js
+++ src/components/association_profile/News/News.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react'
-import {Link} from "react-router-dom"
+import React, { Component } from 'react'
+import { Link } from "react-router-dom"
 
 const $ = require("jquery");
 $.DataTable = require("datatables.net");
@@ -9,82 +9,95 @@ class News extends Component {
 
     state = {
         isLoading: true,
-        photos: null
+        news: []
     }
     async componentDidMount() {
-      const url = "https://jsonplaceholder.typicode.com/photos"
-      const response = await fetch(url)
-      const data = await response.json()
-      this.setState({ photos: data, isLoading: false });
-      $(function () {
-          $('#example').DataTable({
-              "pagingType": "full_numbers",
-              "lengthMenu": [[5, 10, 20, -1], [5, 10, 25, "All"]]
-          });
-    
-          
-      });
-      
-    }
-    render(){
-        const { photos, isLoading } = this.state;
+        const url = "http://fva-backend-dev.herokuapp.com/api/app/news/"
+        const response = await fetch(url)
+        const data = await response.json()
+        this.setState({ news: data.results, isLoading: false });
+        console.log(this.state.news);
+        $(function () {
+            $('#example').DataTable({
+                "pagingType": "full_numbers",
+                "lengthMenu": [[5, 10, 20, -1], [5, 10, 25, "All"]]
+            });
 
-        if (isLoading || !photos) {
+
+        });
+    }
+
+    deleteEvent(NewsId){
+            const currentNews = this.state.news
+            this.setState({
+                news: currentNews.filter(news => news.id !== NewsId),
+              });
+            const url='http://fva-backend-dev.herokuapp.com/api/app/news/'+NewsId
+             fetch(url, {
+                method : "DELETE"
+            })
+    }
+
+    render() {
+        const { news, isLoading } = this.state;
+
+        if (isLoading || !news) {
             return <div className="container">Loading ... </div>
         }
         else {
-        return (
-            <div className="container">
-    
-                <h1>News</h1>
-                <Link to='/AddNews'><button className="add-new-button">Add New</button></Link>
-    
-                <div className="meta-table">
-    
+            return (
+                <div className="container">
+
+                    <h1>News</h1>
+                    <Link to='/AddNews'><button className="add-new-button">Add New</button></Link>
+
+                    <div className="meta-table">
+
                         <div className="main-menu">
-                            <h2 className="active" style={{width: '12rem'}}><i className="fa fa-newspaper-o"></i>News List</h2>     
+                            <h2 className="active" style={{ width: '12rem' }}><i className="fa fa-newspaper-o"></i>News List</h2>
                         </div>
 
-                </div>
-    
-                
-    
-                <table id="example" className="table-User hover display compact row-border hover order-column" style={{ width: '100%' }}>
-     
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Title</th>
-                            <th>Content</th>
-                            <th>Main Image</th>
-                            <th>Thumbnail Image</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
+                    </div>
 
-                   
-                    <tbody>
-                    {this.state.photos.slice(0,5).map((photo, i) => (
-                        <tr key={i}>
-                            <td>{photo.id}</td>
-                            <td>Technology</td>
-                            <td>{photo.title}</td>
-                            <td>{photo.url}</td>
-                            <td>{photo.thumbnailUrl}</td>
-                            <td className="actions-table">
-                                <i className="fas fa-edit"></i>
-                                <i className="fas fa-trash"></i>
-                            </td>
-                        </tr>
-                    ))
-                    }
-                </tbody> 
-                </ table>
-            </div>
-        )
+
+
+                    <table id="example" className="table-User hover display compact row-border hover order-column" style={{ width: '100%' }}>
+
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Title</th>
+                                <th>Content</th>
+                                <th>Main Image</th>
+                                <th>Thumbnail Image</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+
+
+                        <tbody>
+                            {news.map((item, i) => {
+                                return (
+                                    <tr key={i}>
+                                        <td>{item.id}</td>
+                                        <td>{item.title}</td>
+                                        <td>{item.content}</td>
+                                        <td>{<img className="img-news-list" alt="" src={item.main_image} />}</td>
+                                        <td>{<img className="img-news-list" alt="" src={item.thumbnail_image} />}</td>
+                                        <td className="actions-table">
+                                            <i className="fas fa-edit"></i>
+                                            <i className="fas fa-trash" onClick={()=>{this.deleteEvent(item.id)}}></i>
+                                        </td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </ table>
+                </div>
+            )
+        }
     }
-    }
-    
+
 }
 
 export default News
